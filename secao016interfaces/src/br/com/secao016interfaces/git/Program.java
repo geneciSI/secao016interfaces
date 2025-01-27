f94ee11e55b49c3cc9cd347ba8dfbd5eff2a1d78
Geneci Filho <85906784+geneciSI@users.noreply.github.com>
2025-01-26 21:19:31 -0300

    Adicionados via upload

---
+++ secao016interfaces/src/br/com/secao016interfaces/git/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.secao016interfaces.git;
+
+/**Exemplo
+ * 
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+			System.out.println("Hello");
+	}
+
+}
